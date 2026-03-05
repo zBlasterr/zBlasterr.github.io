df0ad69d61926fcddba80590ab15f9347fd90e34
Guilherme Santos <100953726+zBlasterr@users.noreply.github.com>
2026-03-05 15:56:58 -0300

    Implement item navigation and auto-update feature
    Added functionality for item navigation and automatic updates.

--- lojaonline/scripts.js
+++ lojaonline/scripts.js
@@ -1,3 +1,48 @@
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
+const itens = document.querySelectorAll('.item')
+const dots = document.querySelectorAll('.dot')
+const numberIndicator = document.querySelector('.numbers')
+const list = document.querySelector('.list')
 
+let active = 0
+const total = itens.length
+let timer;
+
+function update(direction) {
+    document.querySelector('.item.active').classList.remove('active')
+    document.querySelector('.dot.active').classList.remove('active')
+
+    if (direction > 0) {
+        active++
+
+        if (active === total) {
+            active = 0
+        }
+    } 
+
+    else if (direction < 0) {
+        active--
+
+        if (active < 0) {
+            active = total - 1
+        }
+    }
+    itens[active].classList.add('active')
+    dots[active].classList.add('active')
+
+    numberIndicator.textContent = '0' + (active + 1)
+}
+
+clearInterval(timer)
+timer = setInterval(() => {
+    update(1)
+}, 5000)
+
+prevButton.addEventListener('click', () => {    
+    update(-1)
+})
+
+nextButton.addEventListener('click', () => {    
+    update(1)
+})
